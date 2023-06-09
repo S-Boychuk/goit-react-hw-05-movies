6dd858786008cc2c790d6639da5c2bf56603bc29
S-Boychuk <svitlanabojchuk95@gmail.com>
2023-06-09 15:04:46 +0300

    Update Section.jsx

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,12 +1,12 @@
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import css from './Section.module.css';
 
 const Section = ({ children }) => {
   return <section className={css.section}>{children}</section>;
 };
 
-// Section.propTypes = {
-//   children: PropTypes.node,
-// };
+Section.propTypes = {
+  children: PropTypes.node,
+};
 
 export default Section;
